62f17b5e415696a237d00dc203fed1655afb3079
zekrom-vale <sgraven66@gmail.com>
2017-07-26 14:41:39 -0500

    optomised code & partly added dropdown options

--- idea.js
+++ idea.js
@@ -1,28 +1,27 @@
 function SET(){
-	var form=[];
-	for(var i=0; document.getElementsByTagName('input').length>i; i++){
-		if(document.getElementsByTagName('input')[i].type!= 'checkbox'){
-			form[window.location.href][i]={
-				"id": document.getElementsByTagName('input')[i].id,
-				"type": document.getElementsByTagName('input')[i].type,
-				"name": document.getElementsByTagName('input')[i].name,
-				"value": document.getElementsByTagName('input')[i].value
-			}
-		else{
-			form[window.location.href][i]={
-				"id": document.getElementsByTagName('input')[i].id,
-				"type": 'checkbox',
-				"checked": document.getElementsByTagName('input')[i].checked,
-				"name": document.getElementsByTagName('input')[i].name
-			}
+	var form=[],
+	elments=document.querySelectorAll('input, textarea, select');
+	for(var i=0; elments.length>i; i++){
+		form[window.location.href][i]={
+			"id": elments[i].id,
+			"type": elments[i].type,
+			"name": elments[i].name
 		}
-	}
-	for(var I=0; document.getElementsByTagName('textarea').length>0; I++){
-		form[window.location.href][i+I+1]={
-			"id": document.getElementsByTagName('textarea')[i].id,
-			"type": document.getElementsByTagName('textarea')[i].type,
-			"name": document.getElementsByTagName('textarea')[i].name,
-			"value": document.getElementsByTagName('textarea')[i].value
+		if(elments[i].type== 'checkbox'){
+			form[window.location.href][i]["checked"]= elments[i].checked;
+		}
+		else if(elments[i].tagName== 'SELECT'){
+			form[window.location.href][i]["value"]= elments[i].value;
+			//Show options
+			form[window.location.href][i]["innerHTML"]= elments[i].innerHTML;
+		}
+		else{
+			//Show options
+			if(elements[i].list!= null){
+				form[window.location.href][i]["list"]= elments[i].getAttribute('list');
+				
+			}
+			form[window.location.href][i]["value"]= elments[i].value;
 		}
 	}
 	chrome.storage.local.set(form);
@@ -30,11 +29,11 @@ function SET(){
 function GET(){
 	chrome.storage.local.get(window.location.href, (items)=>{
 		for(var i=0; items[window.location.href].length>0; i++){
-			if(items[window.location.href][i][type]!= 'checkbox'){
-				document.getElementById(items[window.location.href][i][id]).value=items[window.location.href][i][value];
+			if(items[window.location.href][i]["type"]!= 'checkbox'){
+				document.getElementById(items[window.location.href][i]["id"]).value=items[window.location.href][i]["value"];
 			}
 			else{
-				document.getElementById(items[window.location.href][i][id]).checked=items[window.location.href][i][checked];
+				document.getElementById(items[window.location.href][i]["id"]).checked=items[window.location.href][i][checked];
 			}
 		}
 	});
